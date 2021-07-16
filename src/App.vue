6e77c802ed1809c633f1f84d96abaabf156a4ad2
parix1999 <parisottoedoardo@gmail.com>
2021-07-16 16:49:47 +0200

    mofiche primarie

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="app">
-
+    <Header />
   </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
+    Header,
+  },
+  created () {
+    
+  },
+  // data: function() {
 
-  }
+  // },
 }
 </script>
 
 <style lang="scss">
+
+@import './style/app.scss';
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <div class="box-logo">
+            <img src="../assets/netflix-logo.png" alt="Logo">
+        </div>
+    </nav>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Header',
+  
+}
+</script>
+
+<style lang="scss">
+
+</style>
